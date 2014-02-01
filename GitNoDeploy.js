592dadd22fd7d2a2473f0b56e65a4ad1a7f44283
Domenico Leone Luciani <domenicoleoneluciani@gmail.com>
2014-02-01 03:39:55 +0100

    Added functions

--- GitNoDeploy.js
+++ GitNoDeploy.js
@@ -3,47 +3,135 @@
 var http        =   require('http');
 var url         =   require('url');
 var querystring =   require('querystring');
+var fs          =   require('fs');
+var exec        =   require('child_process').exec;
 var config      =   require('./GitNoDeploy-config.json');
 
 // Constructor
-var GitNoDeploy = function() {
-  this.port = config.port || 2001;
+var GitNoDeploy = function () {
+  this.port   = config.port   ||  2001;
+  this.quiet  = config.quiet  ||  false;
   this.init();
 };
 
-// Function to execute pull action to your repositories
-GitNoDeploy.prototype.deploy = function(postData) {
-  console.log("Arrivato: "+postData.ciao);
-}
+//Executes command after pull request
+GitNoDeploy.prototype.afterExec = function (index) {
+  var self = this;
+  config.repos[index].after.forEach(function (value, pos) {
+    exec(value, function (error, stdout, stderr) {
+      if (error === null && !self.quiet) {
+        console.log("[" + value + "] - (" + stdout + ")");
+      }
+      else {
+        if(!self.quiet) {
+          console.log("[" + value + "] - (\n" + stdout + ") - {\n" + stderr + "}");
+        }
+      }
+    });
+  });
+};
+
+GitNoDeploy.prototype.pull = function (index) {
+  var self = this;
+  exec('cd ' + config.repos[index].path + ' && git pull', function (error, stdout, stderr) {
+    if (error === null) {
+      if (!self.quiet) {
+        console.log(config.repos[index].url + " updated!");
+      }
+      //If present execute after commands
+      if (Array.isArray(config.repos[index].after)) {
+        self.afterExec(index);
+      }
+      else {
+        if (!self.quiet) {
+          console.log("After is not an array!");
+        }
+      }
+    }
+    else {
+      if (!self.quiet) {
+        console.log("Error on pull request: " + error + "\n" + stdout + "\n" + stderr);
+      }
+    }
+  });
+};
+
+// Function to check Repos
+GitNoDeploy.prototype.checkRepos = function (index) {
+
+  if (fs.existsSync(config.repos[index].path)) {
+    if (fs.existsSync(config.repos[index].path + "/.git")) {
+      this.pull(index);
+    }
+    else {
+      if (!this.quiet) {
+        console.log(config.repos[index].path + " Is not a repository");
+      }
+    }
+  }
+  else {
+    if (!this.quiet) {
+      console.log(config.repos[index].path + " Not found on your filesystem!");
+    }
+  }
+};
+
+// Check repository
+GitNoDeploy.prototype.deploy = function (postData) {
+  var self = this;
+  var find = false;
+
+  config.repos.forEach(function (value, index) {
+    if (postData.repository.url === value.url) {
+      find = true;
+      self.checkRepos(index);
+    }
+  });
+
+  if (!this.quiet) {
+    if (find === false ) {
+      console.log("Repository not found!");
+    }
+    else {
+      console.log("Repository found!");
+    }
+  }
+};
 
 // Check Server Request
 function checkRequest(req, res) {
-  var self = this;
-  switch(req.url) {
+  var self = this, query = '';
+  switch (req.url) {
     case '/':
-      if(req.method === 'POST') {
-        var query = '';
+      if (req.method === 'POST') {
         req.on('data', function(data) {
           query += data;
         });
         req.on('end', function() {
-          res.writeHeader(200, {"Content-Type": "text/plain"});
+          res.writeHeader(200, {'Content-Type': 'text/plain'});
           res.end();
-          var postQuery = JSON.parse(querystring.parse(query).payload);
-          if(postQuery) {
-            // Request sent to me
+          var postQuery = JSON.parse(querystring.parse(query,'&','=',{'maxKeys': 1}).payload);
+          if (postQuery) {
             self.deploy(postQuery);
+          }
+          else {
+            if (!self.quiet) {
+              console.log("Query didn't accepted!");
+            }
           }
         });
       }
-      break;
+    break;
   }
 }
 
-// Init
-GitNoDeploy.prototype.init = function() {
+// Start HTTP Server
+GitNoDeploy.prototype.init = function () {
   http.createServer(checkRequest.bind(this)).listen(this.port);
-  console.log("Server Listened on port: "+this.port);
+  if (!this.quiet) {
+    console.log("Server Listened on port: "+this.port);
+  }
 };
 
+//Go
 var Go = new GitNoDeploy();
